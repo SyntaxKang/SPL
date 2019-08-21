7d232328a679548ce84562f1243391cd34fec3c3
syntax <parna8441@naver.com>
2019-08-21 12:52:21 +0900

    로그인후 화면 추가

--- src/main/java/com/spl/spl/controller/controller.java
+++ src/main/java/com/spl/spl/controller/controller.java
@@ -6,17 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class controller {
 
+    //메인화면
     @GetMapping("/index")
-    public String index()
-    {
-        return "index";
-    }
+    public String index(){ return "index";}
+    //로그인성공시 화면
+    @GetMapping("/index2")
+    public String index2(){ return "index2";}
+    //모음클릭시 화면
     @GetMapping("/")
     public String timeline(){
         return "timeline";
     }
-    @GetMapping("/RecordRoom")
+    //기록실 클릭시 화면
+    @GetMapping("/RecodeRoom")
     public String RecordRoom(){
-        return "RecordRoom";
+        return "RecodeRoom";
+    }
+
+
+    @GetMapping("/Bowling/Recode")
+    public String Recode(){
+        return "/Bowling/RecodeRoom";
+    }
+    @GetMapping("/Bowling/MVP")
+    public String Mvp(){
+        return "Mvp";
     }
 }
