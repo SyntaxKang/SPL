961dbc8ffd83d33c313d7109de71024f7d8ee663
NaSangYeon <sktkddus2@naver.com>
2019-08-30 16:40:44 +0900

    controller.java 파일 수정본

--- src/main/java/com/spl/spl/controller/controller.java
+++ src/main/java/com/spl/spl/controller/controller.java
@@ -1,17 +1,36 @@
 package com.spl.spl.controller;
 
+import com.spl.spl.dto.group.Groups;
+import com.spl.spl.service.group.GroupServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@AllArgsConstructor
 public class controller {
+
+    private GroupServiceImpl groupService;
 
     //메인화면
     @GetMapping("/index")
     public String index(){ return "index";}
     //로그인성공시 화면
     @GetMapping("/index2")
-    public String index2(){ return "index2";}
+    public String index2(Model model){
+        List<Groups> groupsList = groupService.findAll();
+
+        for (Groups groups : groupsList) {
+            System.out.println(groups.getGroupIdx());
+        }
+
+        model.addAttribute("groupsList", groupsList);
+
+        return "index2";
+    }
     //모음클릭시 화면
     @GetMapping("/")
     public String timeline(){
@@ -20,9 +39,13 @@ public class controller {
     //기록실 클릭시 화면
     @GetMapping("/RecodeRoom")
     public String RecordRoom(){
-        return "RecodeRoom";
+        return "record/RecodeRoom";
     }
 
+    @GetMapping("/table")
+    public String table(){
+        return "tables";
+    }
 
     @GetMapping("/Bowling/Recode")
     public String Recode(){
